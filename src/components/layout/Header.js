814c49cd960939deb999e3d744950c885f8a9395
osmanakkus44 <osmanakkus44@gmail.com>
2020-11-01 19:44:04 +0300

    redux store creation and hook up with AddProject

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
           <div className="container">
-            <a className="navbar-brand" href="/dashboard">
+            <a className="navbar-brand" href="/">
               Personal Project Management Tool
             </a>
             <button
--- src/components/project/AddProject.js
+++ src/components/project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -29,14 +32,13 @@ class AddProject extends Component {
       startDate: this.state.startDate,
       endDate: this.state.endDate,
     };
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
     return (
       <div>
         {}
-
         <div className="project">
           <div className="container">
             <div className="row">
@@ -108,4 +110,8 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createProject })(AddProject);
